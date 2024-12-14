5396ded6436d9b7be87a5c1bd496200f335870ee
huyvanzzz <23020379@vnu.edu.vn>
2024-12-14 22:58:14 +0700

    commit 4

---
+++ procedure.sql
@@ -0,0 +1,27 @@
+-- tính tổng doanh thu trong một tháng theo năm.
+DELIMITER $$
+
+CREATE PROCEDURE CalculateMonthlyRevenue(IN p_Year INT, IN p_Month INT)
+BEGIN
+    DECLARE totalRevenue DECIMAL(10, 2);
+
+    -- Tính tổng doanh thu của tháng từ Payments
+    SELECT IFNULL(SUM(P.PaymentAmount),0)
+    INTO totalRevenue
+    FROM Payments P
+    JOIN Orders O ON P.OrderID = O.OrderID
+    WHERE YEAR(P.PaymentDate) = p_Year
+    AND MONTH(P.PaymentDate) = p_Month;
+
+    -- Hiển thị doanh thu
+    SELECT totalRevenue AS MonthlyRevenue;
+    IF totalRevenue = 0 THEN
+        SELECT 'Không có doanh thu trong tháng này.' AS Message;
+    ELSE
+        SELECT CONCAT('Doanh thu tháng ', p_Month, ' năm ', p_Year, ' là: ', totalRevenue) AS Message;
+    END IF;
+END$$
+
+DELIMITER ;
+-- Gọi hàm để tính tổng doanh thu tháng 9 năm 2024.
+CALL CalculateMonthlyRevenue(2024, 9);
